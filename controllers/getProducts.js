1cd6bb333860785ce975935bcf50a1549acf4564
ram kumar <rammaniyari@gmail.com>
2023-09-03 12:13:13 +0530

    get all product router is created'

--- controllers/getProducts.js
+++ controllers/getProducts.js
@@ -4,15 +4,25 @@ const Products = require("../models/project");
 
 // define route handler
 
-exports.getProducts = async(req,res)=>{
+exports.getProducts = async (req, res) => {
     try {
-        
+
+        // fetch all products items from database
+        const products = await Products.find({});
+
+        // response
+        return res.status(200).json({
+            success: true,
+            data: products,
+            message: "Entire products data is feteched"
+        })
+
     } catch (error) {
         console.log(error);
         return res.status(500).json({
-            success : false,
-            error : error.message,
-            message : "Internal server error"
+            success: false,
+            error: error.message,
+            message: "Internal server error"
         })
     }
 }
--- router/productRouter.js
+++ router/productRouter.js
@@ -6,8 +6,14 @@ const router = express.Router();
 // importing create router
 const {create }  = require("../controllers/createProduct");
 
+// import getProducts controller
+const {getProducts} = require("../controllers/getProducts");
+
 // to create add product router
 router.post("/create",create);
+
+// get all products router
+router.get("/",getProducts);
 
 // exporting router
 module.exports = router;
