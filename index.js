92cb314c2d79df503e8faa885601bd4f00113dfb
ram kumar <rammaniyari@gmail.com>
2023-09-03 11:32:37 +0530

    adding router in app

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ const express = require("express");
 require("dotenv").config();
 const port = process.env.port || 8080;
 
+// creating server
+const app = express();
+
+// adding middleware
+app.use(express.json());
+
 // importing connect function to connect mongodb compass
 const connect = require("./config/db");
 
-// creating server
-const app = express();
+// importing  router file
+const productRouter = require("./router/productRouter");
+app.use("/",productRouter);
 
 // connecting mongodb compass
 connect();
 
-
-
-
-
-
 // activating server
-
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 })
---
+++ router/productRouter.js
@@ -0,0 +1,9 @@
+
+// importing express package to create router
+const express = require("express");
+const router = express.Router();
+
+
+
+// exporting router
+module.exports = router;
